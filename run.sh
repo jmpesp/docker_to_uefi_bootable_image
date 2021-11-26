5a322f6de55bb8b33d4f4451186f84495bf37e29
James MacMahon <jwm@operand.ca>
2021-11-26 16:24:47 -0500

    bare bones debian tester

--- run.sh
+++ run.sh
@@ -3,11 +3,14 @@ set -e
 
 make
 
+docker build -t debian-tester -f Dockerfile.debian-tester --no-cache .
+
 sudo \
     ./target/debug/docker_to_uefi_bootable_image \
     create \
-        --image-name debian:latest \
+        --image-name debian-tester \
         --output-file debian.img \
         --disk-size 2 \
-        --root-passwd nNGQlzZxBYxBmPIgpEP5ezgbqPb4L2R4
+        --root-passwd nNGQlzZxBYxBmPIgpEP5ezgbqPb4L2R4 \
+        --flavor debian
 
